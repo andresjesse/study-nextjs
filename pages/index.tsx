11430c12d7d558d5f1a19a21ac26c6dbab2110c0
Andres <andres@pop-os.localdomain>
2022-06-09 15:13:35 -0300

    another change

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,6 +16,7 @@ const Home: NextPage = () => {
       <main className={styles.main}>
         <h1 className={styles.title}>Hello Next</h1>
         <div>New Content</div>
+        <div>Another one</div>
       </main>
 
       <footer className={styles.footer}></footer>
